d339cb81b35451a17f02a1982e8a2a08266200e8
Just A Coder <61443129+mellowpuppy@users.noreply.github.com>
2020-10-26 15:55:17 -0400

    simplified code

--- index.php
+++ index.php
@@ -4,23 +4,18 @@
 <title>Stack Evolve</title>
 
 // need to call css and js
+// also need site icon
 
 </head>
 <body>
 
+    // main page content
     <?php
-      //calling python script
-      $command = escapeshellcmd('/usr/custom/search.py');
-      $output = shell_exec($command);
-      echo $output;
-
-      include "relative/path/to/your/firstfile.html";
-      include "relative/path/to/your/secondfile.html";
-      include "relative/path/to/your/evenwithothersuffix/thirdfile.php";
-      include "relative/path/to/your/fourth/file/in/another/folder.html";
+      include "/header.php";
+      include "/body.php";
+      include "/footer.php";
     ?>
-    
- // minimal js code here
 
+    
 </body>
 </html> 
