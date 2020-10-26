69034138c7aa4de97bf570b4a109a85d0874c839
Just A Coder <61443129+mellowpuppy@users.noreply.github.com>
2020-10-26 15:52:59 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Stack Evolve</title>
+
+// need to call css and js
+
+</head>
+<body>
+
+    <?php
+      //calling python script
+      $command = escapeshellcmd('/usr/custom/search.py');
+      $output = shell_exec($command);
+      echo $output;
+
+      include "relative/path/to/your/firstfile.html";
+      include "relative/path/to/your/secondfile.html";
+      include "relative/path/to/your/evenwithothersuffix/thirdfile.php";
+      include "relative/path/to/your/fourth/file/in/another/folder.html";
+    ?>
+    
+ // minimal js code here
+
+</body>
+</html> 
