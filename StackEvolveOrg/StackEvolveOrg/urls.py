08cc9b26040b1ac3f3ffda3629bbba2c219e1247
Just A Coder <61443129+mellowpuppy@users.noreply.github.com>
2021-02-11 21:58:52 -0500

    urls and ignore pycache

--- StackEvolveOrg/StackEvolveOrg/urls.py
+++ StackEvolveOrg/StackEvolveOrg/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('about/', include('polls.urls')),
+    path('about/', include('about.urls')),
     path('admin/', admin.site.urls),
 ]
