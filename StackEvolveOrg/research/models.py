3e594902a73a3360553cc9fe6e4bf70d958f4d47
Just A Coder <61443129+mellowpuppy@users.noreply.github.com>
2021-02-12 20:51:08 -0500

    begin author and article models

--- StackEvolveOrg/research/models.py
+++ StackEvolveOrg/research/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+class Reporter(models.Model):
+    full_name = models.CharField(max_length=70)
+
+    def __str__(self):
+        return self.full_name
+
 # Create your models here.
+class Article(models.Model):
+    pub_date = models.Datefield()
+    headline = models.CharField(max_length=200)
+    content = models.Textfield()
+    author = models.ForeignKey(Reporter, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.headline
