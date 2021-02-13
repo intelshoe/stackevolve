e42099f3a90ac58476b06946e1f461c280107e70
Just A Coder <61443129+mellowpuppy@users.noreply.github.com>
2021-02-12 21:24:44 -0500

    set research urls

--- StackEvolveOrg/research/urls.py
+++ StackEvolveOrg/research/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<str:tag>/', views.tag_archive),
+    path('<str:tag>/<str:url_title>', views.article_details)
 ]
