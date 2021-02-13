956862b10682dace2975fa76cb9e7efdc520af23
Just A Coder <61443129+mellowpuppy@users.noreply.github.com>
2021-02-12 21:16:48 -0500

    added tags for searching

--- StackEvolveOrg/research/models.py
+++ StackEvolveOrg/research/models.py
@@ -14,6 +14,7 @@ class Article(models.Model):
     # not safe
     url_title = models.Textfield(max_legnth=25)
     content = models.Textfield()
+    tags = models.Textfield()
     author = models.ForeignKey(Reporter, on_delete=models.CASCADE)
 
     def __str__(self):
