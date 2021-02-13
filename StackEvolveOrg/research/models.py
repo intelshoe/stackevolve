856fed592a2cbc4c255743c9138356a7dcf9c91e
Just A Coder <61443129+mellowpuppy@users.noreply.github.com>
2021-02-12 21:11:36 -0500

    research articles cont

--- StackEvolveOrg/research/models.py
+++ StackEvolveOrg/research/models.py
@@ -1,15 +1,18 @@
 from django.db import models
 
+# name of person who submitted article
 class Reporter(models.Model):
     full_name = models.CharField(max_length=70)
 
     def __str__(self):
         return self.full_name
 
-# Create your models here.
+# Research articles
 class Article(models.Model):
     pub_date = models.Datefield()
     headline = models.CharField(max_length=200)
+    # not safe
+    url_title = models.Textfield(max_legnth=25)
     content = models.Textfield()
     author = models.ForeignKey(Reporter, on_delete=models.CASCADE)
 
