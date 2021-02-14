b85f8bb8e29c69c5fd115fe8a09f9477b45f5d21
Just A Coder <61443129+mellowpuppy@users.noreply.github.com>
2021-02-13 20:31:34 -0500

    homepage

--- StackEvolveOrg/StackEvolveOrg/views.py
+++ StackEvolveOrg/StackEvolveOrg/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+
 def index(request):
-    return HttpResponse("Welcome to our researching, \
-        and rodeo studio!")
+    return render(request, 'index/comingsoon.html')
