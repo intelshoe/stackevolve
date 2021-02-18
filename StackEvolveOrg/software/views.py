04f9ae9d02bd5b383e175bb02613734cc0664cee
Just A Coder <61443129+mellowpuppy@users.noreply.github.com>
2021-02-17 22:04:58 -0500

    added software and questions pages

--- StackEvolveOrg/software/views.py
+++ StackEvolveOrg/software/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 # Create your views here.
 def index(request):
-    return HttpResponse("Here you can download, preview, and contribute to \
-        our amazing software projects!")
+    return render(request, 'software/index.html')
