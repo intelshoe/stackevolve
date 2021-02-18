23d90d513b0c711cf04507092b74358e917f1618
Just A Coder <61443129+mellowpuppy@users.noreply.github.com>
2021-02-17 21:47:51 -0500

    about page works!

--- StackEvolveOrg/about/views.py
+++ StackEvolveOrg/about/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Welcome to the about us and contact page!")
+    return render(request, 'about/index.html')
